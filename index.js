9b0c182555408e781d8503d599cad7f8ee4cb042
John Tekpor Dzikunu <john.dzikunu@amalitech.org>
2022-09-12 18:40:09 +0000

    finial work

--- index.js
+++ index.js
@@ -1,44 +1,25 @@
 // Import the express lirbary
 const express = require('express')
+const axios = require('axios').default;
+const app = express();
 
-// Import the axios library, to make HTTP requests
-const axios = require('axios')
+app.get('/', async (req, res) => {
 
-// This is the client ID and client secret that you obtained
-// while registering the application
-const clientID = '<your client id>'
-const clientSecret = '<your client secret>'
-
-// Create a new express application and use
-// the express static middleware, to serve all files
-// inside the public directory
-const app = express()
-app.use(express.static(__dirname + '/public'))
-
-app.get('/oauth/redirect', (req, res) => {
-  // The req.query object has the query params that
-  // were sent to this route. We want the `code` param
-  const requestToken = req.query.code;
+  const code = req.query.code;
   const clientID = req.query.clientID;
   const clientSecret = req.query.clientSecret;
   axios({
-    // make a POST request
     method: 'post',
-    // to the Github authentication API, with the client ID, client secret
-    // and request token
-    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
-    // Set the content type header, so that we get the response in JSOn
+    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${code}`,
     headers: {
-      accept: 'application/json'
+         accept: 'application/json'
     }
   }).then((response) => {
-    // Once we get the response, extract the access token from
-    // the response body
-    const accessToken = response.data.access_token
-    // redirect the user to the welcome page, along with the access token
-    res.json(accessToken);
+    res.json(response.data);
+  }).catch(error => {
+    res.json(error);
   })
-})
-
+});
 // Start the server on port 8080
+
 app.listen(process.port|8080)
