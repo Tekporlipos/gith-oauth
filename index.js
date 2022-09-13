e703157bee071c351971f14240a4f153da4ca42a
John Tekpor Dzikunu <john.dzikunu@amalitech.org>
2022-09-13 07:40:10 +0000

    lyrics added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 var cors = require('cors')
 const axios = require('axios').default;
 const app = express();
+const lyricsFinder = require('lyrics-finder');
 
 app.use(express.static("public"))
 app.use(cors());
@@ -21,6 +22,15 @@ app.get('/api/v1/auth/', async (req, res) => {
   }).catch(error => {
     res.json(error);
   })
+});
+
+app.get('/lyrics', async (req, res) => {
+  const artist = req.query.artist;
+  const title = req.query.title;
+  (async function(artist, title) {
+    let lyrics = await lyricsFinder(artist, title) || "Not Found!";
+    res.json(lyrics);
+})(artist, title)
 });
 // Start the server on port 8080
 
